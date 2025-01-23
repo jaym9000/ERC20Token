8a59ce0399ccd98455c7e585bc4ee0f0d6d7a5bf
Jean-Marc R-Mahoro <jean_marcc@hotmail.com>
2025-01-22 23:01:33 -0400

    updated source and test file, 9 successful tests

--- src/ERCToken.sol
+++ src/ERCToken.sol
@@ -15,9 +15,7 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
  * Originally based on code by FirstBlood: https://github.com/Firstbloodio/token/blob/master/smart_contract/FirstBloodToken.sol
  */
 
-
 contract ERCToken is ERC20, ERC20Burnable, Ownable, ERC20Permit {
-
     /*//////////////////////////////////////////////////////////////
                                FUNCTIONS
     //////////////////////////////////////////////////////////////*/
@@ -25,11 +23,9 @@ contract ERCToken is ERC20, ERC20Burnable, Ownable, ERC20Permit {
     /// @notice Contract constructor initializes the token with name, symbol and owner
     /// @param initialOwner The address that will be granted ownership of the contract
     /// @dev Sets up ERC20 with "EzID" name and "EID" symbol, and initializes permit functionality
-    constructor(address initialOwner)
-        ERC20("EzID", "EID")
-        Ownable(initialOwner)
-        ERC20Permit("EzID")
-    {}
+    constructor(
+        address initialOwner
+    ) ERC20("EzID", "EID") Ownable(initialOwner) ERC20Permit("EzID") {}
 
     /// @notice Allows the owner to mint new tokens
     /// @param to The address that will receive the minted tokens
@@ -38,46 +34,4 @@ contract ERCToken is ERC20, ERC20Burnable, Ownable, ERC20Permit {
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
-
-    /*//////////////////////////////////////////////////////////////
-                                GETTERS
-    //////////////////////////////////////////////////////////////*/
-
-    /// @notice Gets the total supply of the token
-    /// @return The total token supply
-    function totalSupply() override public view returns (uint256) {}
-
-    /// @notice Gets the balance of a specific address
-    /// @param _owner The address to query the balance of
-    /// @return balance The token balance of the specified address
-    function balanceOf(address _owner) override public view returns (uint256 balance){}
-
-    /// @notice Transfers tokens to a specified address
-    /// @param _to The address to transfer to
-    /// @param _value The amount to be transferred
-    /// @return success True if the transfer was successful
-    function transfer(address _to, uint256 _value) override public returns (bool success){}
-
-    /// @notice Transfer tokens from one address to another
-    /// @param _from The address which you want to transfer tokens from
-    /// @param _to The address which you want to transfer to
-    /// @param _value The amount of tokens to be transferred
-    /// @return success True if the transfer was successful
-    function transferFrom(address _from, address _to, uint256 _value) override public returns (bool success){}
-
-    /// @notice Approve the passed address to spend the specified amount of tokens
-    /// @param _spender The address which will spend the funds
-    /// @param _value The amount of tokens to be spent
-    /// @return success True if the approval was successful
-    function approve(address _spender, uint256 _value) override public returns (bool success){}
-
-    /// @notice Check the amount of tokens that an owner allowed to a spender
-    /// @param _owner The address which owns the funds
-    /// @param _spender The address which will spend the funds
-    /// @return remaining The amount of remaining tokens allowed to spent
-    function allowance(address _owner, address _spender) override public view returns (uint256 remaining){}
-
-    // event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    // event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-
 }
--- test/TokenTest.t.sol
+++ test/TokenTest.t.sol
@@ -6,8 +6,83 @@ import {ERCToken} from "../../src/ERCToken.sol";
 
 contract TokenTest is Test {
     ERCToken public token;
+    address public owner;
+    address public user1;
+    address public user2;
+    uint256 public constant INITIAL_MINT_AMOUNT = 1000e18; // 1000 tokens
+    uint256 public constant TRANSFER_AMOUNT = 100e18; // 100 tokens
 
     function setUp() public {
-        token = new ERCToken(msg.sender);
+        owner = address(this);
+        user1 = makeAddr("user1");
+        user2 = makeAddr("user2");
+
+        // Deploy token with this contract as owner
+        token = new ERCToken(owner);
+
+        // Mint initial tokens to owner
+        token.mint(owner, INITIAL_MINT_AMOUNT);
+    }
+
+    function test_InitialSetup() public view {
+        assertEq(token.name(), "EzID");
+        assertEq(token.symbol(), "EID");
+        assertEq(token.decimals(), 18);
+        assertEq(token.owner(), owner);
+    }
+
+    function test_Minting() public {
+        uint256 mintAmount = 500e18;
+        token.mint(user1, mintAmount);
+        assertEq(token.balanceOf(user1), mintAmount);
+        assertEq(token.totalSupply(), INITIAL_MINT_AMOUNT + mintAmount);
+    }
+
+    function test_RevertWhen_MintingByNonOwner() public {
+        vm.prank(user1);
+        vm.expectRevert();
+        token.mint(user1, 100e18);
+    }
+
+    function test_Transfer() public {
+        token.transfer(user1, TRANSFER_AMOUNT);
+        assertEq(token.balanceOf(user1), TRANSFER_AMOUNT);
+        assertEq(token.balanceOf(owner), INITIAL_MINT_AMOUNT - TRANSFER_AMOUNT);
+    }
+
+    function test_TransferFrom() public {
+        // Approve user1 to spend tokens
+        token.approve(user1, TRANSFER_AMOUNT);
+
+        // User1 transfers tokens from owner to user2
+        vm.prank(user1);
+        token.transferFrom(owner, user2, TRANSFER_AMOUNT);
+
+        assertEq(token.balanceOf(user2), TRANSFER_AMOUNT);
+        assertEq(token.balanceOf(owner), INITIAL_MINT_AMOUNT - TRANSFER_AMOUNT);
+    }
+
+    function test_Burning() public {
+        token.burn(TRANSFER_AMOUNT);
+        assertEq(token.balanceOf(owner), INITIAL_MINT_AMOUNT - TRANSFER_AMOUNT);
+        assertEq(token.totalSupply(), INITIAL_MINT_AMOUNT - TRANSFER_AMOUNT);
+    }
+
+    function test_Allowance() public {
+        token.approve(user1, TRANSFER_AMOUNT);
+        assertEq(token.allowance(owner, user1), TRANSFER_AMOUNT);
+    }
+
+    function test_RevertWhen_TransferInsufficientBalance() public {
+        vm.expectRevert();
+        token.transfer(user1, INITIAL_MINT_AMOUNT + 1);
+    }
+
+    function test_RevertWhen_TransferFromInsufficientAllowance() public {
+        token.approve(user1, TRANSFER_AMOUNT);
+
+        vm.prank(user1);
+        vm.expectRevert();
+        token.transferFrom(owner, user2, TRANSFER_AMOUNT + 1);
     }
 }
