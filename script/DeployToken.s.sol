e38bbd2b7cc9565f4ee3c79a317d54f9c166c6be
Jean-Marc R-Mahoro <jean_marcc@hotmail.com>
2025-01-23 21:00:52 -0400

    installed yarn

--- script/DeployToken.s.sol
+++ script/DeployToken.s.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.22;
 import {Script} from "forge-std/Script.sol";
 import {ERCToken} from "../../src/ERCToken.sol";
 
-contract DeployToken is Script{
+contract DeployToken is Script {
     function deploy() public {
         new ERCToken(msg.sender);
     }
